7e2c7ae72f0a4e41dcbc99514500271b9cb80113
luxidea <contact@luxidea.net>
2017-11-14 10:10:11 +0900

    no message

--- BezierConverter.dpr
+++ BezierConverter.dpr
@@ -4,8 +4,6 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   Main in 'Main.pas' {Form1},
-  LUX.Lattice.T2 in '_LIBRARY\LUXOPHIA\LUX\LUX.Lattice.T2.pas',
-  LUX.Lattice.T3 in '_LIBRARY\LUXOPHIA\LUX\LUX.Lattice.T3.pas',
   LUX.M2 in '_LIBRARY\LUXOPHIA\LUX\LUX.M2.pas',
   LUX.M3 in '_LIBRARY\LUXOPHIA\LUX\LUX.M3.pas',
   LUX.M4 in '_LIBRARY\LUXOPHIA\LUX\LUX.M4.pas',
@@ -20,7 +18,6 @@ uses
   LUX.D4.M4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D4.M4.pas',
   LUX.D4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D4.pas',
   LUX.D4.V4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D4.V4.pas',
-  LUX.Lattice.T1 in '_LIBRARY\LUXOPHIA\LUX\LUX.Lattice.T1.pas',
   LUX.D5 in '_LIBRARY\LUXOPHIA\LUX\LUX.D5.pas',
   LUX.Curve.T1.D1 in '_LIBRARY\LUXOPHIA\LUX.Curve\LUX.Curve.T1.D1.pas',
   LUX.Curve.T1.D2 in '_LIBRARY\LUXOPHIA\LUX.Curve\LUX.Curve.T1.D2.pas',
@@ -29,8 +26,12 @@ uses
   LUX.Curve.T2.D2 in '_LIBRARY\LUXOPHIA\LUX.Curve\LUX.Curve.T2.D2.pas',
   LUX.Curve.T2.D3 in '_LIBRARY\LUXOPHIA\LUX.Curve\LUX.Curve.T2.D3.pas',
   FrameGraph in 'FrameGraph.pas' {GraphFrame: TFrame},
-  LUX.Tree in '_LIBRARY\LUXOPHIA\LUX\LUX.Tree.pas',
-  LUX.DN in '_LIBRARY\LUXOPHIA\LUX\LUX.DN.pas';
+  LUX.DN in '_LIBRARY\LUXOPHIA\LUX\LUX.DN.pas',
+  LUX.Data.Octree in '_LIBRARY\LUXOPHIA\LUX\LUX.Data.Octree.pas',
+  LUX.Data.Tree in '_LIBRARY\LUXOPHIA\LUX\LUX.Data.Tree.pas',
+  LUX.Data.Lattice.T1 in '_LIBRARY\LUXOPHIA\LUX\LUX.Data.Lattice.T1.pas',
+  LUX.Data.Lattice.T2 in '_LIBRARY\LUXOPHIA\LUX\LUX.Data.Lattice.T2.pas',
+  LUX.Data.Lattice.T3 in '_LIBRARY\LUXOPHIA\LUX\LUX.Data.Lattice.T3.pas';
 
 {$R *.res}
 
